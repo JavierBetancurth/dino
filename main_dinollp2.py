4abfe4253bc5ee95540294aaf57e23a2042ed46c
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-11-11 01:55:56 -0500

    Update main_dinollp2.py

--- main_dinollp2.py
+++ main_dinollp2.py
@@ -460,7 +460,7 @@ class ProportionLoss(nn.Module):
         """
         # Asegurarse de que `inputs` sean probabilidades (p)
         # inputs = F.softmax(inputs, dim=-1)
-        # inputs = torch.clamp(inputs, self.epsilon, 1 - self.epsilon)
+        inputs = torch.clamp(inputs, self.epsilon, 1 - self.epsilon)
         # Calcular la pérdida de Cross Entropy
         ce_loss = -torch.sum(targets * torch.log(inputs), dim=-1)
         return ce_loss.mean()
@@ -471,7 +471,7 @@ class ProportionLoss(nn.Module):
         """
         # Asegurarse de que `targets` estén normalizados como probabilidades (q)
         # targets = F.normalize(targets, p=1, dim=-1)
-        # targets = torch.clamp(targets, self.epsilon, 1 - self.epsilon)
+        targets = torch.clamp(targets, self.epsilon, 1 - self.epsilon)
         # Calcular la pérdida de Reverse Cross Entropy
         rce_loss = -torch.sum(inputs * torch.log(targets), dim=-1)
         return rce_loss.mean()
